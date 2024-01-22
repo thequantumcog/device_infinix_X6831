a21c6cd85648da65dbc852d4dc5814b2de3dc383
ar0177417 <ar0177417@gmail.com>
2024-01-22 18:38:38 +0500

    Test

--- twrp_X6831.mk
+++ twrp_X6831.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 
