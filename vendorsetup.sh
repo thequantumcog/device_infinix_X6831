74e8e01712d8ad944b2ea9db7babc454763a9371
ar0177417 <ar0177417@gmail.com>
2024-01-24 18:19:50 +0500

    Disable ccache

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,18 +34,18 @@ export OF_STATUS_INDENT_RIGHT=48
 export OF_ALLOW_DISABLE_NAVBAR=0
 export OF_CLOCK_POS=1
 
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-export CCACHE_MAXSIZE="5G"
-export CCACHE_DIR="~/ccache"
-
-if [ ! -d ${CCACHE_DIR} ];
-then
-  echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-  echo "Please edit the CCACHE_DIR build variable or mount the directory."
-fi
-
-export LC_ALL="C"
+# export USE_CCACHE=1
+# export CCACHE_EXEC=/usr/bin/ccache
+# export CCACHE_MAXSIZE="5G"
+# export CCACHE_DIR="~/ccache"
+#
+# if [ ! -d ${CCACHE_DIR} ];
+# then
+#   echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+#   echo "Please edit the CCACHE_DIR build variable or mount the directory."
+# fi
+#
+# export LC_ALL="C"
 
 # Clone to fix build on minimal manifest
 git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
