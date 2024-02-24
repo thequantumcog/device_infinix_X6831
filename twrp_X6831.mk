a2ce9925c6e7caef0e1d559f79b9dba3e5dc2e85
Ahmad Rehan <67918673+ardrag0n@users.noreply.github.com>
2024-02-24 11:26:51 +0500

    Update twrp_X6831.mk

--- twrp_X6831.mk
+++ twrp_X6831.mk
@@ -6,7 +6,7 @@
 #
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
