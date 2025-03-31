1d1139c814d34d68ca764ed77255196be97809c1
TegarXLu <botdimas67@gmail.com>
2025-03-31 11:18:06 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,4 +183,53 @@ TW_DEVICE_VERSION := ${shell git -C $(DEVICE_PATH) rev-parse --short HEAD}-Tegar
 #TWRP_INCLUDE_LOGCAT := true
 #TARGET_USES_LOGD := true
 
+#SHRP Prop
+
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/infinix/X6831
+
+# Maintainer name *
+SHRP_MAINTAINER := TegarXLu
+
+# Device codename *
+SHRP_DEVICE_CODE := X6831
+
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+# SHRP Padding Flag (Only for rounded corner devices.) [Optional]
+# You have to change these values according to your device's roundness.
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+
+# For Notch devices [Optional]
+SHRP_NOTCH := true
+
+# put this 0 if device has no EDL mode *
+SHRP_EDL_MODE := 0
+
+# Put your device's paths from fstab *
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+
+# Use this flag only if your device is A/B *
+SHRP_AB := true
+
+# Check your device's recovery path, dont use blindly
+SHRP_REC := no_recovery_partition 
+
+# Put 0 to disable flashlight
+SHRP_FLASH := 1
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/torch/torch/torch_level
+SHRP_FONP_2 := /sys/class/sub_torch/sub_torch/sub_torch_level
+SHRP_FONP_3 := /sys/class/flashlight_core/flashlight/flashlight_torch
+
+# Default (if not set) will use /system if SHRP_EXPRESS true otherwise will use legacy method of patching
+SHRP_EXPRESS_USE_DATA := true
+
 
