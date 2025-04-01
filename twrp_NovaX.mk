e76eec61e104f27c283f346a0f7188230a970576
TegarXLu <botdimas67@gmail.com>
2025-04-01 10:49:59 +0700

    Update and rename twrp_X6831.mk to twrp_NovaX.mk

--- twrp_NovaX.mk
+++ twrp_NovaX.mk
@@ -13,14 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from X6812 device
-$(call inherit-product, device/infinix/X6831/device.mk)
+$(call inherit-product, device/infinix/NovaX/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := X6831
-PRODUCT_NAME := twrp_X6831
+PRODUCT_DEVICE := NovaX
+PRODUCT_NAME := twrp_NovaX
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix HOT 30
 PRODUCT_MANUFACTURER := infinix
