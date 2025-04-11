a67453aff34324bfa8efe36e04bf64afaefe9a54
TegarXLu <botdimas67@gmail.com>
2025-04-11 18:28:05 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,3 +198,7 @@ TW_DEVICE_VERSION := TegarXLu | NovaX >a13<
 # Debug
 #TWRP_INCLUDE_LOGCAT := true
 #TARGET_USES_LOGD := true
+
+#PBRP_FLAGS
+PB_TORCH_PATH := "/sys/devices/virtual/torch/torch/torch_level"
+PB_TORCH_MAX_BRIGHTNESS := 1
